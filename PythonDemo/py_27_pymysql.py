40328de824e24329983f59f3bb30e8fb201a42f0
xiaohan-1122 <zhangxiaohankid@163.com>
2020-10-14 23:26:07 +0800

    pymysql1

--- PythonDemo/py_27_pymysql.py
+++ PythonDemo/py_27_pymysql.py
@@ -4,3 +4,34 @@
 # @File: py_27_pymysql.py
 # @Author: xiaohanzhang
 # @Data: 2020/10/13
+
+import pymysql
+
+def main():
+    # 创建连接对象
+    conn = pymysql.connect(host='47.92.123.248',
+                           port=3306,
+                           user='root',
+                           password='A262408_a',
+                           database='test_database',
+                           charset='utf8')
+    # 获取游标
+    cursor = conn.cursor()
+    # sql语句
+    sql = 'select * from students'
+    # 执行sql语句
+    cursor.execute(sql)
+    # 获取查询结果
+    # row = cursor.fetchone()
+    # print(row)
+    data_tuple = cursor.fetchall()
+    for row in data_tuple:
+        print(row)
+    # 关闭游标
+    cursor.close()
+    # 关闭连接
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
