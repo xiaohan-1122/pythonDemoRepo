8332a622d8cc8c89fdd2334982524ae2c9a0ea8a
xiaohan <zhangxiaohan_qd@126.com>
2021-09-07 09:55:05 +0800

    file

--- PythonDemo/python基础/py20_file.py
+++ PythonDemo/python基础/py20_file.py
@@ -118,7 +118,7 @@ file_write.close()
 import os
 """
 文件操作：
-rename      重命名文件       os.rename(源文件名, 目标文件名)
+rename      重命名文件或目录       os.rename(源文件名, 目标文件名)
 remove      删除文件         os.remove(文件名)
 path.isfile 判断是否是文件    os.path.isfile(路径)
 
@@ -126,7 +126,7 @@ path.isfile 判断是否是文件    os.path.isfile(路径)
 listdir     目录列表        os.listdir(目录名)
 mkdir       创建目录        os.mkdir(目录名)
 rmdir       删除目录        os.rmdir(目录名)
-getcwd      获取当前目录     os.getcwd()
+getcwd      获取当前目录路径     os.getcwd()
 chdir       修改工作目录     os.chdir(目标目录)
 path.isdir  判断是否是目录   os.path.isdir(路径)
 """
